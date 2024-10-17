d0392e43453cda537caa48a5f511734ed19e380c
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2024-10-18 00:08:26 +0100

    Skeleton final

--- src/components/posts/PostsLoadingSkeleton.tsx
+++ src/components/posts/PostsLoadingSkeleton.tsx
@@ -1,13 +1,13 @@
 import { Skeleton } from "../ui/skeleton";
 
 export default function PostsLoadingSkeleton() {
-    return (
-        <div className="space-y-5">
-            <PostLoadingSkeleton/>
-            <PostLoadingSkeleton/>
-            <PostLoadingSkeleton/>
-        </div>
-    )
+  return (
+    <div className="space-y-5">
+      <PostLoadingSkeleton />
+      <PostLoadingSkeleton />
+      <PostLoadingSkeleton />
+    </div>
+  );
 }
 
 function PostLoadingSkeleton() {
@@ -20,11 +20,13 @@ function PostLoadingSkeleton() {
           <Skeleton className="h-4 w-20 rounded" />
         </div>
       </div>
-          <Skeleton className="h-6 w-72 rounded" />
-          <Skeleton className="h-6 w-52 rounded" />
+      <div className="space-y-1.5">
+        <Skeleton className="h-4 w-[90%] rounded" />
+        <Skeleton className="h-4 w-[60%] rounded" />
+      </div>
       <div className="grid grid-cols-2 gap-2">
-          <Skeleton className="aspect-square rounded-2xl" />
-          <Skeleton className="aspect-square rounded-2xl" />
+        <Skeleton className="aspect-square rounded-2xl" />
+        <Skeleton className="aspect-square rounded-2xl" />
       </div>
       <div className="flex justify-between">
         <Skeleton className="h-6 w-12 rounded" />
