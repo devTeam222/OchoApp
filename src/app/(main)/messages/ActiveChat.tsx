505a807f905ca8be8e07eee007607984f9455eda
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2024-11-03 16:03:53 +0100

    Saved issue

--- src/app/(main)/messages/ActiveChat.tsx
+++ src/app/(main)/messages/ActiveChat.tsx
@@ -87,6 +87,7 @@ export default function ActiveChat({
   const loggedMember = channel.members.find(
     (member) => member.userId === loggedUser.id,
   );
+  const isSaved = channel.id === `saved-${loggedMember?.userId}`;
   const isMember = !(
     loggedMember?.type === "OLD" || loggedMember?.type === "BANNED"
   );
@@ -161,12 +162,15 @@ export default function ActiveChat({
       </InfiniteScrollContainer>
 
       <div className="max-sm:bg-card/50">
-        {!channel.isGroup && !otherUser?.id && (
-          <div className="select-none px-5 py-1.5 text-center text-sm">
-            <p>{message}</p>
-            <p>L&apos;utilisateur n&apos;est plus disponible</p>
-          </div>
-        )}
+        {!isSaved
+          ? !channel.isGroup &&
+            !otherUser?.id && (
+              <div className="select-none px-5 py-1.5 text-center text-sm">
+                <p>{message}</p>
+                <p>L&apos;utilisateur n&apos;est plus disponible</p>
+              </div>
+            )
+          : !!channelId && <MessageForm channelId={channelId} />}
         {!isMember ? (
           <div className="select-none px-5 py-1.5 text-center text-sm">
             <p>{message}</p>
