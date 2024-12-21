ef36323fa2d07ec802d68fb922cfa4f1d321b979
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2024-12-21 07:44:40 +0100

    Folower duplicate fix

--- src/app/api/users/followers/route.ts
+++ src/app/api/users/followers/route.ts
@@ -4,34 +4,38 @@ import { getUserDataSelect } from "@/lib/types";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-    try {
-        const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
-        const pageSize = 10;
+  try {
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+    const pageSize = 10;
 
-        const { user } = await validateRequest();
+    const { user } = await validateRequest();
 
-        if (!user) {
-            return Response.json({ error: "Action non autorisée" }, { status: 401 });
-        }
-
-        // Récupérer les followers
-        const followers = await prisma.user.findMany({
-            where: {
-                followers: { some: { followerId: user.id } },
-            },
-            take: pageSize + 1,
-            cursor: cursor ? { id: cursor } : undefined,
-            orderBy: { id: 'asc' },
-            select: getUserDataSelect(user.id),
-        });
-
-        const nextCursor = followers.length > pageSize ? followers[pageSize].id : null;
-        const followersPage = followers.length > pageSize ? followers.slice(0, pageSize) : followers;
-
-        return Response.json({ users: followersPage, nextCursor });
-        
-    } catch (error) {
-        console.error(error);
-        return Response.json({ error: "Internal server error" }, { status: 500 });
+    if (!user) {
+      return Response.json({ error: "Action non autorisée" }, { status: 401 });
     }
+
+    // Récupérer les followers
+    const followers = await prisma.user.findMany({
+      where: {
+        AND: [
+          { followers: { some: { followerId: user.id } } },
+          { NOT: { following: { some: { followingId: user.id } } } },
+        ],
+      },
+      take: pageSize + 1,
+      cursor: cursor ? { id: cursor } : undefined,
+      orderBy: { id: "asc" },
+      select: getUserDataSelect(user.id),
+    });
+
+    const nextCursor =
+      followers.length > pageSize ? followers[pageSize].id : null;
+    const followersPage =
+      followers.length > pageSize ? followers.slice(0, pageSize) : followers;
+
+    return Response.json({ users: followersPage, nextCursor });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
 }
--- src/app/api/users/following/route.ts
+++ src/app/api/users/following/route.ts
@@ -4,34 +4,38 @@ import { getUserDataSelect } from "@/lib/types";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-    try {
-        const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
-        const pageSize = 10;
+  try {
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+    const pageSize = 10;
 
-        const { user } = await validateRequest();
+    const { user } = await validateRequest();
 
-        if (!user) {
-            return Response.json({ error: "Action non autorisée" }, { status: 401 });
-        }
-
-        // Récupérer les suivis
-        const following = await prisma.user.findMany({
-            where: {
-                following: { some: { followingId: user.id } },
-            },
-            take: pageSize + 1,
-            cursor: cursor ? { id: cursor } : undefined,
-            orderBy: { id: 'asc' },
-            select: getUserDataSelect(user.id),
-        });
-
-        const nextCursor = following.length > pageSize ? following[pageSize].id : null;
-        const followingPage = following.length > pageSize ? following.slice(0, pageSize) : following;
-
-        return Response.json({ users: followingPage, nextCursor });
-        
-    } catch (error) {
-        console.error(error);
-        return Response.json({ error: "Internal server error" }, { status: 500 });
+    if (!user) {
+      return Response.json({ error: "Action non autorisée" }, { status: 401 });
     }
+
+    // Récupérer les suivis
+    const following = await prisma.user.findMany({
+      where: {
+        AND: [
+          { following: { some: { followingId: user.id } } },
+          { NOT: { followers: { some: { followerId: user.id } } } },
+        ],
+      },
+      take: pageSize + 1,
+      cursor: cursor ? { id: cursor } : undefined,
+      orderBy: { id: "asc" },
+      select: getUserDataSelect(user.id),
+    });
+
+    const nextCursor =
+      following.length > pageSize ? following[pageSize].id : null;
+    const followingPage =
+      following.length > pageSize ? following.slice(0, pageSize) : following;
+
+    return Response.json({ users: followingPage, nextCursor });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
 }
