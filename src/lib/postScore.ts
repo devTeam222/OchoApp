8cc0c43fef41b9234f13a8efc71c6714aa78284d
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2024-12-25 20:52:41 +0100

    Refactor le calcul des scores de pertinence des publications en utilisant le cache pour améliorer les performances

--- src/lib/postScore.ts
+++ src/lib/postScore.ts
@@ -1,58 +1,57 @@
-import { validateRequest } from "@/auth";
+import { cache } from "react";
 import prisma from "./prisma";
-import {
-  getCommentDataIncludes,
-  getPostDataIncludes,
-  PostData,
-  UserData,
-} from "./types";
+import { getPostDataIncludes, PostData, UserData } from "./types";
 
-export async function calculateRelevanceScore(
-  post: PostData,
-  user: UserData,
-  latestPostId?: string, // Paramètre facultatif
-): Promise<number> {
-  const postId = post.id;
-  const userId = user.id;
+export const calculateRelevanceScore = cache(
+  async (
+    post: PostData,
+    user: UserData,
+    latestPostId?: string, // Paramètre facultatif
+  ): Promise<number> => {
+    const postId = post.id;
+    const userId = user.id;
 
-  // Récupération des commentaires associés au post
-  const comments = await prisma.comment.findMany({
-    where: {
-      postId,
-    },
-    include: getCommentDataIncludes(userId),
-  });
+    // Récupération des commentaires associés au post
+    const comments = await prisma.comment.findMany({
+      where: {
+        postId,
+      },
+      select: {
+        id: true,
+      },
+    });
 
-  const now = new Date();
-  const timeFactor =
-    1 / (1 + (now.getTime() - post.createdAt.getTime()) / (1000 * 60 * 60)); // Pondération pour les posts récents
-  const engagementScore =
-    post.likes.length * 2 + comments.length * 3 + post.bookmarks.length * 1.5;
-  const proximityScore = post.user.followers.some(
-    (follower) => follower.followerId === user.id,
-  )
-    ? 5
-    : 0; // Bonus si l'utilisateur suit l'auteur
-  const typeFactor =
-    post.attachments.length > 0 && post.content.length ? 1.5 : 1; // Bonus si le post contient des médias
+    const now = new Date();
+    const timeFactor =
+      1 / (1 + (now.getTime() - post.createdAt.getTime()) / (1000 * 60 * 60)); // Pondération pour les posts récents
+    const engagementScore =
+      post.likes.length * 2 + comments.length * 3 + post.bookmarks.length * 1.5;
+    const proximityScore = post.user.followers.some(
+      (follower) => follower.followerId === userId,
+    )
+      ? 5
+      : 0; // Bonus si l'utilisateur suit l'auteur
+    const typeFactor =
+      post.attachments.length > 0 && post.content.length ? 1.5 : 1; // Bonus si le post contient des médias
 
-  const gradientFactor =
-    !post.attachments.length && post.content.length < 100 && post.gradient
-      ? 1.5
-      : 1; // Bonus si le post a un gradient et peu de contenu
+    const gradientFactor =
+      !post.attachments.length && post.content.length < 100 && post.gradient
+        ? 1.5
+        : 1; // Bonus si le post a un gradient et peu de contenu
 
-  // Priorisation explicite pour le post le plus récent (si fourni)
-  const latestPostBonus = latestPostId && post.id === latestPostId ? 100 : 0;
+    // Priorisation explicite pour le post le plus récent (si fourni)
+    const latestPostBonus = latestPostId && post.id === latestPostId ? 100 : 0;
 
-  return (
-    engagementScore * timeFactor +
-    proximityScore +
-    typeFactor +
-    gradientFactor +
-    latestPostBonus
-  );
-}
-export async function calculateAndStoreScoresForUser(user: UserData) {
+    return (
+      engagementScore * timeFactor +
+      proximityScore +
+      typeFactor +
+      gradientFactor +
+      latestPostBonus
+    );
+  },
+);
+export const calculateAndStoreScoresForUser = cache(async (user: UserData) => {
   const userId = user.id;
   const posts = await prisma.post.findMany({
     include: getPostDataIncludes(userId),
@@ -71,4 +70,4 @@ export async function calculateAndStoreScoresForUser(user: UserData) {
       });
     }),
   );
-}
+});
