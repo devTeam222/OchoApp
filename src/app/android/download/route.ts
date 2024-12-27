826599663fe468790a007b9b03e50d9482139c54
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2024-12-27 23:51:05 +0100

    Ajout d'une route pour le téléchargement de l'APK de l'application Android avec gestion des erreurs et types de contenu appropriés.

---
+++ src/app/android/download/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+
+// Répertoire de base pour les fichiers téléchargés
+const downloadBaseDir = path.resolve("data/downloads");
+
+export async function GET(request: NextRequest) {
+  const fileName = "OchoApp.apk"
+  try {
+    // Construire le chemin complet du fichier
+    const filePath = path.join(downloadBaseDir, fileName);
+
+    // Vérifier si le fichier existe
+    if (!fs.existsSync(filePath)) {
+      return NextResponse.json({ error: "File not found" }, { status: 404 });
+    }
+
+    // Lire le fichier dans un buffer
+    const fileBuffer = await fs.promises.readFile(filePath);
+
+    // Détecter le type de contenu en fonction de l'extension du fichier
+    const ext = path.extname(filePath).toLowerCase();
+    let contentType = "application/octet-stream"; // Type par défaut
+    if (ext === ".apk") contentType = "application/vnd.android.package-archive";
+
+    // Retourner le fichier avec le type de contenu approprié
+    return new NextResponse(fileBuffer, {
+      headers: {
+        "Content-Type": contentType,
+        "Content-Disposition": `attachment; filename="${path.basename(filePath)}"`, // Pour forcer le téléchargement
+      },
+    });
+  } catch (error) {
+    console.error("Error serving file:", error);
+    return NextResponse.json({ error: "Error retrieving file" }, { status: 500 });
+  }
+}
