c2a4678193b3a4134e0e30b0270a838b9fea1ab4
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2025-01-20 14:31:29 +0100

    Amélioration de la récupération des posts : ajout des trois derniers posts triés par date, ajustement de la logique de pertinence et exclusion des posts déjà récupérés.

--- src/app/api/posts/following/route.ts
+++ src/app/api/posts/following/route.ts
@@ -8,49 +8,48 @@ export async function GET(req: NextRequest) {
   try {
     const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
 
-    const pageSize = 5;
-
     const { user } = await validateRequest();
 
     if (!user) {
       return Response.json({ error: "Action non autorisée" }, { status: 401 });
     }
 
-    const posts = await prisma.post.findMany({
-      where: {
-        user: {
-          followers: {
-            some: {
-              followerId: user.id,
-            },
-          },
-        },
-      },
+    // Récupérer les trois derniers posts triés par date
+    const latestPosts = await prisma.post.findMany({
       include: getPostDataIncludes(user.id),
-      orderBy: { createdAt: "desc" },
+      orderBy: {
+        createdAt: "desc",
+      },
+      take: !cursor ? 3 : 0,
+    });
+
+    const pageSize = 5 + latestPosts.length
+
+    // Récupérer les posts suivants triés par pertinence
+    const relevantPosts = await prisma.post.findMany({
+      include: getPostDataIncludes(user.id),
+      orderBy: [
+        {
+          relevanceScore: "desc",
+        },
+        {
+          createdAt: "desc",
+        },
+      ],
       take: pageSize + 1,
       cursor: cursor ? { id: cursor } : undefined,
+      where: {
+        id: {
+          notIn: latestPosts.map(post => post.id), // Exclure les posts déjà récupérés
+        },
+      },
     });
 
-    const postsWithScores = posts.slice(0, pageSize).map((post) => {
-      return {
-        ...post,
-        relevanceScore: calculateRelevanceScore(
-          post,
-          user,
-          posts[0]?.id || undefined,
-        ),
-      };
-    });
-
-    const sortedPosts = postsWithScores
-      .sort((a, b) => b.relevanceScore - a.relevanceScore)
-      .map(({ relevanceScore, ...post }) => post);
-
-    const nextCursor = posts.length > pageSize ? posts[pageSize].id : null;
+    const allPosts = [...latestPosts, ...relevantPosts];
+    const nextCursor = allPosts.length > pageSize + 3 ? allPosts[pageSize + 3].id : null;
 
     const data: PostsPage = {
-      posts: sortedPosts,
+      posts: allPosts.slice(0, pageSize), // Limiter le nombre de posts retournés
       nextCursor,
     };
 
