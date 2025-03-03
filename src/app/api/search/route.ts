b1c6d75c9a8e3917ac5a16a708d94c78ed4af0f3
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2025-03-03 09:15:35 +0100

    Suppression de la condition userId dans la vérification des utilisateurs vérifiés dans l'API de recherche

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -148,9 +148,7 @@ export async function GET(req: NextRequest) {
              
               {
                 verified: {
-                  some: {
-                    userId: user.id,
-                  },
+                  some: {}
                 },
               },
               {
