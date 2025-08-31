5e2da9e2f09d5d7a9d4432acab6a5ec86ec0359a
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2025-08-31 04:32:08 +0100

    feat: ajouter la fonctionnalité de création de posts avec validation et gestion des sessions

---
+++ src/app/api/android/posts/new/route.ts
@@ -0,0 +1,62 @@
+import prisma from "@/lib/prisma";
+import { getPostDataIncludes } from "@/lib/types";
+import { createPostSchema } from "@/lib/validation";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const authHeader = req.headers.get("Authorization");
+    const sessionToken = authHeader?.split(" ")[1];
+
+    if (!sessionToken) {
+      return NextResponse.json({
+        success: false,
+        message: "Action non autorisée",
+      });
+    }
+
+    const session = await prisma.session.findUnique({
+      where: {
+        id: sessionToken,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!session?.user) {
+      return NextResponse.json({
+        success: false,
+        message: "Action non autorisée",
+      });
+    }
+
+    const input = await req.json();
+
+    const { content, mediaIds, gradient } = createPostSchema.parse(input);
+
+    const newPost = await prisma.post.create({
+      data: {
+        content,
+        userId: session.user.id,
+        gradient,
+        attachments: {
+          connect: mediaIds.map((id: string) => ({ id })),
+        },
+      },
+      include: getPostDataIncludes(session.user.id),
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Post publié avec succès.",
+      data: newPost,
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      success: false,
+      message: "Quelque chose s'est mal passé. Veuillez réessayer.",
+    });
+  }
+}
