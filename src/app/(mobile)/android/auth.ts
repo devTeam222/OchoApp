39a6d29346513f1cfe55701d8af1c8eb20a2b71d
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2025-09-01 23:12:28 +0100

    fix: corriger l'URI de redirection pour l'authentification GitHub

--- src/app/(mobile)/android/auth.ts
+++ src/app/(mobile)/android/auth.ts
@@ -9,6 +9,6 @@ export const github = new GitHub(
   process.env.GITHUB_CLIENT_ID!,
   process.env.GITHUB_CLIENT_SECRET!,
   {
-    redirectURI: `${process.env.NEXT_PUBLIC_BASE_URL}/api/callback/github/android`,
+    redirectURI: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback/github/android`,
   },
 );
