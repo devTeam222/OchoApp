ae04016123112ba48a9b28a8b5b1ea657dcc984f
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2025-09-15 12:52:08 +0100

    feat: ajouter la fonctionnalité de bookmark pour les posts avec gestion des sessions utilisateur

---
+++ src/app/api/android/posts/[postId]/bookmark/route.ts
@@ -0,0 +1,88 @@
+// /api/android/posts/[postId]/like/route.ts
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { ApiResponse } from "../../../utils/dTypes";
+
+export async function POST(
+  req: NextRequest,
+  { params: { postId } }: { params: { postId: string } },
+) {
+  try {
+    const headersList = headers();
+    const authorization = headersList.get("authorization");
+
+    if (!authorization) {
+      return NextResponse.json({
+        success: false,
+        message: "Unauthorized: Missing Authorization header.",
+      } as ApiResponse<null>);
+    }
+
+    const sessionToken = authorization.replace("Bearer ", "");
+    const session = await prisma.session.findUnique({
+      where: { id: sessionToken },
+      include: { user: true },
+    });
+
+    if (!session || !session.user) {
+      return NextResponse.json({
+        success: false,
+        message: "Unauthorized: Invalid session.",
+      } as ApiResponse<null>);
+    }
+
+    const userId = session.user.id;
+    let isBookmarked = false;
+
+    // Utilise une transaction pour garantir que les opérations sont atomiques
+    await prisma.$transaction(async (prisma) => {
+      const existingBookmark = await prisma.bookmark.findUnique({
+         where: {
+                userId_postId: {
+                    userId,
+                    postId
+                }
+            },
+      });
+
+      if (existingBookmark) {
+        // Si le like existe, on le supprime
+        await prisma.bookmark.delete({
+          where: {
+            userId_postId: { userId, postId },
+          },
+        });
+        isBookmarked = false;
+      } else {
+        // Sinon, on le crée
+        await prisma.bookmark.create({
+          data: {
+            postId,
+            userId,
+          },
+        });
+
+        // Crée une notification si l'utilisateur n'est pas l'auteur du post
+        const post = await prisma.post.findUnique({
+          where: { id: postId },
+          select: { userId: true },
+        });
+        isBookmarked = true;
+      }
+    });
+
+
+    return NextResponse.json({
+      success: true,
+      message: "Post bookmarked successfully.",
+      data: { isBookmarked },
+    } as ApiResponse<{ isBookmarked: boolean; }>);
+  } catch (error) {
+    console.error("Error in like endpoint:", error);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong. Please try again.",
+    } as ApiResponse<null>);
+  }
+}
