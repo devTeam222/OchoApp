b7c529daf0efd79d78e05c1709014e8212e4c079
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2025-09-22 16:02:24 +0200

    Ajout de la logique pour modifier le profil utilisateur avec vérification des autorisations et gestion des en-têtes d'appareil.

--- src/app/api/android/users/[userId]/route.ts
+++ src/app/api/android/users/[userId]/route.ts
@@ -156,3 +156,150 @@ export async function GET(
     } as ApiResponse<null>);
   }
 }
+
+// Endpoint pour la modification de profil utilisateur
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { userId: string } },
+) {
+  try {
+    const authHeader = req.headers.get("Authorization");
+    const session_token = authHeader?.split(" ")[1];
+    const session = await prisma.session.findFirst({
+      where: {
+        id: session_token,
+      },
+      select: {
+        user: {
+          select: {
+            id: true,
+            username: true,
+            displayName: true,
+            avatarUrl: true,
+            bio: true,
+            lastSeen: true,
+            createdAt: true,
+            following: {
+              select: {
+                followerId: true,
+              },
+              take: 0,
+            },
+            followers: {
+              select: {
+                followerId: true,
+              },
+              take: 0,
+            },
+            verified: true,
+            _count: true,
+          },
+        },
+      },
+    });
+    const currentUser: UserData | undefined = session?.user;
+
+    if (!currentUser) {
+      return NextResponse.json({
+        success: false,
+        message: "Action non autorisée",
+        name: "authorization",
+        data: null,
+      } as ApiResponse<null>);
+    }
+
+    const deviceId = req.headers.get("X-Device-ID");
+    const deviceTypeHeader = req.headers.get("X-Device-Type");
+
+    if (!deviceId || !deviceTypeHeader) {
+      return NextResponse.json({
+        success: false,
+        message: "En-têtes d'appareil manquants (X-Device-ID, X-Device-Type).",
+        name: "missing_device_headers",
+      });
+    }
+
+    const device = await prisma.device.findFirst({
+      where: {
+        deviceId,
+      },
+    });
+
+    const isDeviceLoggedIn = device?.logged;
+
+    if (!isDeviceLoggedIn) {
+      return NextResponse.json({
+        success: false,
+        message: "Appareil non autorisé. Veuillez vous reconnecter.",
+        name: "authorization",
+        data: null,
+      } as ApiResponse<null>);
+    }
+
+    const userId = session?.user.id;
+
+    if (currentUser.id !== userId) {
+      return NextResponse.json({
+        success: false,
+        message: "You can only update your own profile.",
+        name: "forbidden",
+        data: null,
+      } as ApiResponse<null>);
+    }
+
+    const { displayName, bio, avatarUrl } = await req.json();
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId },
+      data: {
+        displayName: displayName ?? currentUser.displayName,
+        bio: bio ?? currentUser.bio,
+        // avatarUrl: avatarUrl ?? currentUser.avatarUrl,
+      },
+      select: getUserDataSelect(currentUser.id),
+    }) as UserData;
+
+    const userVerifiedData = updatedUser.verified?.[0];
+
+    const expiresAt = userVerifiedData?.expiresAt;
+    const canExpire = !!(expiresAt ? new Date(expiresAt).getTime() : null);
+
+    const expired = canExpire && expiresAt ? new Date() < expiresAt : false;
+
+    const isVerified = !!userVerifiedData && !expired;
+
+    const verified: VerifiedUser = {
+      verified: isVerified,
+      type: userVerifiedData?.type,
+      expiresAt: userVerifiedData?.expiresAt,
+    };
+    const isFollowing = updatedUser.followers.some(follower=>follower.followerId === currentUser.id)
+
+    const finalUser: User = {
+      id: updatedUser.id,
+      username: updatedUser.username,
+      displayName: updatedUser.displayName,
+      avatarUrl: updatedUser.avatarUrl || undefined,
+      bio: updatedUser.bio || undefined,
+      verified: verified,
+      createdAt: updatedUser.createdAt.getTime(),
+      lastSeen: updatedUser.lastSeen.getTime(),
+      followersCount: updatedUser?._count.followers || 0,
+      postsCount: updatedUser?._count.posts || 0,
+      isFollowing,
+    };
+
+    return NextResponse.json({
+      success: true,
+      message: "Profile updated successfully",
+      data: finalUser,
+    } as ApiResponse<User>);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      success: false,
+      message: "An unexpected error occurred",
+      name: "unknown",
+    } as ApiResponse<null>);
+  }
+}
