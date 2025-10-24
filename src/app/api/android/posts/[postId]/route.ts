f42c6380bc22f3fa898a8b91c61072c5c3fa605b
devTeam222 <ochokom@ochotouchsolution.onmicrosoft.com>
2025-10-24 16:18:40 +0100

    Ajoute des types pour les réponses API et améliore la gestion des erreurs dans les fonctions GET et DELETE des posts.

--- src/app/api/android/posts/[postId]/route.ts
+++ src/app/api/android/posts/[postId]/route.ts
@@ -1,13 +1,19 @@
 import { NextRequest } from "next/server";
 import prisma from "@/lib/prisma";
-import { ApiResponse } from "../../utils/dTypes";
+import {
+  ApiResponse,
+  Attachment,
+  Post,
+  User,
+  VerifiedUser,
+} from "../../utils/dTypes";
 import { getPostDataIncludes, UserData } from "@/lib/types";
 import { calculateRelevanceScore } from "@/lib/postScore";
 import { get } from "http";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { postId: string } }
+  { params }: { params: { postId: string } },
 ) {
   try {
     const { postId } = params;
@@ -15,11 +21,9 @@ export async function GET(
     const sessionToken = authHeader?.split(" ")[1];
 
     if (!sessionToken) {
-      return Response.json(
-        { success: false, message: "Action non autorisée" }
-      );
+      return Response.json({ success: false, message: "Action non autorisée" });
     }
-    
+
     const session = await prisma.session.findUnique({
       where: {
         id: sessionToken,
@@ -30,13 +34,11 @@ export async function GET(
     });
 
     if (!session?.user) {
-      return Response.json(
-        { success: false, message: "Action non autorisée" }
-      );
+      return Response.json({ success: false, message: "Action non autorisée" });
     }
     const user = { ...session.user } as unknown as UserData;
 
-     // 1. Récupérer les informations de l'appareil à partir des en-têtes
+    // 1. Récupérer les informations de l'appareil à partir des en-têtes
     const deviceId = req.headers.get("X-Device-ID");
     const deviceTypeHeader = req.headers.get("X-Device-Type");
 
@@ -50,7 +52,7 @@ export async function GET(
     }
     const device = await prisma.device.findFirst({
       where: {
-        deviceId
+        deviceId,
       },
     });
     const isDeviceLoggedIn = device?.logged;
@@ -65,84 +67,134 @@ export async function GET(
       } as ApiResponse<null>);
     }
 
-  const [allScores, post] = await prisma.$transaction([
-    prisma.postUserScore.findMany({
-      where: {
-        postId: postId,
-      },
-      select: {
-        userId: true,
-        relevanceScore: true,
-      },
-    }),
-    prisma.post.findUnique({
-      where: {
-        id: postId,
-      },
-      include: getPostDataIncludes(user.id),
-    }),
-  ]);
+    const [allScores, post] = await prisma.$transaction([
+      prisma.postUserScore.findMany({
+        where: {
+          postId: postId,
+        },
+        select: {
+          userId: true,
+          relevanceScore: true,
+        },
+      }),
+      prisma.post.findUnique({
+        where: {
+          id: postId,
+        },
+        include: getPostDataIncludes(user.id),
+      }),
+    ]);
 
-  if (!allScores || !post) {
-    return Response.json({}, { status: 404 });
-  }
+    if (!allScores || !post) {
+      return Response.json({}, { status: 404 });
+    }
 
-  const newUserScore = calculateRelevanceScore(post, user);
+    const newUserScore = calculateRelevanceScore(post, user);
 
-  const postScore =
-    newUserScore +
-    allScores
-      .filter((score) => score.userId !== user.id)
-      .reduce((acc, score) => acc + score?.relevanceScore, 0);
+    const postScore =
+      newUserScore +
+      allScores
+        .filter((score) => score.userId !== user.id)
+        .reduce((acc, score) => acc + score?.relevanceScore, 0);
 
-  await prisma.$transaction([
-    prisma.post.update({
-      where: {
-        id: postId,
-      },
-      data: {
-        relevanceScore: postScore,
-      },
-    }),
-    prisma.postUserScore.upsert({
-      where: {
-        postId_userId: {
+    await prisma.$transaction([
+      prisma.post.update({
+        where: {
+          id: postId,
+        },
+        data: {
+          relevanceScore: postScore,
+        },
+      }),
+      prisma.postUserScore.upsert({
+        where: {
+          postId_userId: {
+            postId,
+            userId: user.id,
+          },
+        },
+        update: {
+          relevanceScore: newUserScore,
+        },
+        create: {
           postId,
           userId: user.id,
+          relevanceScore: newUserScore,
         },
-      },
-      update: {
-        relevanceScore: newUserScore,
-      },
-      create: {
-        postId,
-        userId: user.id,
-        relevanceScore: newUserScore,
-      },
-    }),
-  ]);
-  return Response.json({ success: true });
+      }),
+    ]);
+    const userVerifiedData = post.user.verified?.[0];
+    const expiresAt = userVerifiedData?.expiresAt?.getTime() || null;
+    const canExpire = !!(expiresAt || null);
+
+    const expired =
+      canExpire && expiresAt ? new Date().getTime() < expiresAt : false;
+
+    const isVerified = !!userVerifiedData && !expired;
+
+    const verified: VerifiedUser = {
+      verified: isVerified,
+      type: userVerifiedData?.type,
+      expiresAt,
+    };
+    const attachments: Attachment[] = post.attachments;
+    const author: User = {
+      id: post.userId,
+      username: post.user.username,
+      displayName: post.user.displayName,
+      avatarUrl: post.user.avatarUrl || undefined,
+      bio: post.user.bio || undefined,
+      verified,
+      createdAt: post.user.createdAt.getTime(),
+      lastSeen: post.user.lastSeen.getTime(),
+    };
+    const createdAt: number = post.createdAt.getTime();
+    const content: string = post.content;
+    const gradient: number | undefined = post.gradient || undefined;
+    const id: string = post.id;
+    const likes = post._count.likes;
+    const comments = post._count.comments;
+    const isLiked = post.likes.some((like) => like.userId === user.id);
+    const isBookmarked = post.bookmarks.some(
+      (bookmark) => bookmark.userId === user.id,
+    );
+    const finalPost: Post = {
+      id,
+      author,
+      content,
+      createdAt,
+      attachments,
+      gradient,
+      likes,
+      comments,
+      isLiked,
+      isBookmarked,
+    };
+    return Response.json({
+      success: true,
+      data: finalPost,
+    } as ApiResponse<Post>);
   } catch (error) {
     console.error("Error getting post:", error);
-    return Response.json(
-      { success: false, message: "Erreur lors de la récupération du post"});
+    return Response.json({
+      success: false,
+      message: "Erreur lors de la récupération du post",
+    });
   }
 }
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { postId: string } }
+  { params }: { params: { postId: string } },
 ) {
   try {
     const authHeader = req.headers.get("Authorization");
     const sessionToken = authHeader?.split(" ")[1];
 
     if (!sessionToken) {
-      return Response.json(
-        { success: false, message: "Action non autorisée" }
-      );
+      return Response.json({ success: false, message: "Action non autorisée" });
     }
-    
+
     const session = await prisma.session.findUnique({
       where: {
         id: sessionToken,
@@ -153,12 +205,10 @@ export async function DELETE(
     });
 
     if (!session?.user) {
-      return Response.json(
-        { success: false, message: "Action non autorisée" }
-      );
+      return Response.json({ success: false, message: "Action non autorisée" });
     }
 
-     // 1. Récupérer les informations de l'appareil à partir des en-têtes
+    // 1. Récupérer les informations de l'appareil à partir des en-têtes
     const deviceId = req.headers.get("X-Device-ID");
     const deviceTypeHeader = req.headers.get("X-Device-Type");
 
@@ -172,7 +222,7 @@ export async function DELETE(
     }
     const device = await prisma.device.findFirst({
       where: {
-        deviceId
+        deviceId,
       },
     });
     const isDeviceLoggedIn = device?.logged;
@@ -190,9 +240,7 @@ export async function DELETE(
     const { postId } = params;
 
     if (!postId) {
-      return Response.json(
-        { success: false, message: "ID de post manquant" }
-      );
+      return Response.json({ success: false, message: "ID de post manquant" });
     }
 
     // On vérifie si l'utilisateur est bien l'auteur du post
@@ -205,14 +253,15 @@ export async function DELETE(
     if (!postToDelete) {
       return Response.json(
         { success: false, message: "Post non trouvé" },
-        { status: 404 }
+        { status: 404 },
       );
     }
 
     if (postToDelete.userId !== session.user.id) {
-      return Response.json(
-        { success: false, message: "Vous n'avez pas la permission de supprimer ce post" }
-      );
+      return Response.json({
+        success: false,
+        message: "Vous n'avez pas la permission de supprimer ce post",
+      });
     }
 
     await prisma.post.delete({
@@ -221,11 +270,15 @@ export async function DELETE(
       },
     });
 
-    return Response.json({ success: true, message: "Post supprimé avec succès" });
+    return Response.json({
+      success: true,
+      message: "Post supprimé avec succès",
+    });
   } catch (error) {
     console.error("Error deleting post:", error);
-    return Response.json(
-      { success: false, message: "Erreur lors de la suppression du post" }
-    );
+    return Response.json({
+      success: false,
+      message: "Erreur lors de la suppression du post",
+    });
   }
 }
